c35a07347b220ca017d5aff296358bd96b0ee6fb
eduardo <ejav97@outlook.com>
2025-11-26 17:07:31 -0600

    added new routes

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('base.html')
+
+@app.route('/addBlog')
+def addBlog():
+    return render_template('addBlog.html')
+
+@app.route('/Blogs')
+def Blogs():
+    return render_template('blogs.html')
